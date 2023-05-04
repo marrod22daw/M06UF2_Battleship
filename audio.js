8174bbedbbfbab75e6d5b7e2ad6041824f60c95b
marrod22daw <marrod22daw@proton.me>
2023-05-05 01:06:10 +0200

    21

--- audio.js
+++ audio.js
@@ -1,19 +1,27 @@
-var audio = new Audio('Media/audio.mp3');
-audio.volume = 0.25;
+window.onload = function() {
+  let audio = new Audio('Media/audio.mp3');
 
-var startButton = document.getElementById('start');
-startButton.addEventListener('click', function() {
-  if (audio.readyState === 4) {
-    audio.play();
-  } else {
-    audio.addEventListener('canplaythrough', function() {
-      audio.play();
-    });
-  }
-});
-
-var stopButton = document.getElementById('stop');
-stopButton.addEventListener('click', function() {
-  audio.pause();
-  audio.currentTime = 0;
-});
+  let startButton = document.getElementById('pirata');
+  let isPlaying = false; 
+  startButton.addEventListener('click', function() {
+    startButton.classList.add('animado');
+    if (isPlaying) {
+      audio.pause();
+      isPlaying = false;
+      startButton.classList.remove('animado');
+      startButton.classList.remove('active'); 
+    } else { 
+      if (audio.readyState === 4) {
+        audio.play();
+        isPlaying = true;
+        startButton.classList.add('active'); 
+      } else {
+        audio.addEventListener('canplaythrough', function() {
+          audio.play();
+          isPlaying = true;
+          startButton.classList.add('active'); 
+        });
+      }
+    }
+  });
+}
